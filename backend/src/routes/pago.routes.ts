4df4361228de8a46cf6740b3e1093e376b7f34c3
moisesdevweb <moises.devweb@gmail.com>
2026-03-09 16:55:26 -0500

    cambio de nombre de variables traidos

--- backend/src/routes/pago.routes.ts
+++ backend/src/routes/pago.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { getCuenta, registrarPago } from '../controllers/pago.controller';
+import { cobrarOrden, getPago } from '../controllers/pago.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 import { requireRol } from '../middlewares/role.middleware';
 import { Rol } from '../types/enums';
 
 const router = Router();
 
-router.get('/orden/:ordenId', authMiddleware, requireRol(Rol.ADMIN, Rol.MESERO), getCuenta);
-router.post('/', authMiddleware, requireRol(Rol.ADMIN, Rol.MESERO), registrarPago);
+router.post('/',    authMiddleware, requireRol(Rol.ADMIN, Rol.MESERO), cobrarOrden);
+router.get('/:id', authMiddleware, requireRol(Rol.ADMIN, Rol.MESERO), getPago);
 
 export default router;

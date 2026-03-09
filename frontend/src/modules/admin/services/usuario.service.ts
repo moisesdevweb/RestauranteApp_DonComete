62a85a9f6f2061d82809f45ca48596542e4bbb17
moisesdevweb <moises.devweb@gmail.com>
2026-03-09 14:38:09 -0500

    trayendo las rutas de backend de usuario

--- frontend/src/modules/admin/services/usuario.service.ts
+++ frontend/src/modules/admin/services/usuario.service.ts
@@ -1,4 +1,5 @@
 import api from '@/lib/axios';
+
 export interface UsuarioPayload {
   nombre: string;
   username: string;
@@ -7,9 +8,12 @@ export interface UsuarioPayload {
   telefono?: string;
 }
 
-export const getUsuarios     = async () => (await api.get('/users')).data.data;
-export const crearUsuario    = async (data: UsuarioPayload) =>
+export const getUsuarios       = async () => (await api.get('/users/todos')).data.data;
+export const crearUsuario      = async (data: UsuarioPayload) =>
   (await api.post('/users', data)).data.data;
-export const editarUsuario   = async (id: number, data: Partial<UsuarioPayload>) =>
+export const editarUsuario     = async (id: number, data: Partial<UsuarioPayload>) =>
   (await api.put(`/users/${id}`, data)).data.data;
-export const desactivarUsuario = async (id: number) => (await api.delete(`/users/${id}`)).data;
+export const desactivarUsuario = async (id: number) =>
+  (await api.delete(`/users/${id}`)).data;
+export const reactivarUsuario  = async (id: number) =>
+  (await api.patch(`/users/${id}/reactivar`)).data.data;

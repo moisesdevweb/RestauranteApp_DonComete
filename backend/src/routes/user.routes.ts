639f8e87838b5c5ddfebe3e6d0e34fa347c8d4b9
moisesdevweb <moises.devweb@gmail.com>
2026-03-09 14:34:32 -0500

    rutas de traer todos y cambiar estado de usuario

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getUsers, crearUser, editarUser, desactivarUser, cambiarPassword } from '../controllers/user.controller';
+import { getUsers, crearUser, editarUser, desactivarUser, cambiarPassword, getAllUsers, reactivarUser } from '../controllers/user.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 import { requireRol } from '../middlewares/role.middleware';
 import { Rol } from '../types/enums';
@@ -8,9 +8,10 @@ const router = Router();
 
 // Solo admin gestiona usuarios
 router.get('/', authMiddleware, requireRol(Rol.ADMIN), getUsers);
+router.get('/todos', authMiddleware, requireRol(Rol.ADMIN), getAllUsers);
 router.post('/', authMiddleware, requireRol(Rol.ADMIN), crearUser);
+router.patch('/:id/reactivar', authMiddleware, requireRol(Rol.ADMIN), reactivarUser);
 router.put('/:id', authMiddleware, requireRol(Rol.ADMIN), editarUser);
 router.delete('/:id', authMiddleware, requireRol(Rol.ADMIN), desactivarUser);
 router.patch('/:id/password', authMiddleware, requireRol(Rol.ADMIN), cambiarPassword);
-
 export default router;
